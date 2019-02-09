64f4541e1a3970dc59faabab72ff35b27f430861
elofturtle <jonatan@osninja.se>
2019-02-09 22:44:55 +0100

    enkla fallet funkar för sammanfattning

---
+++ elof.py
@@ -0,0 +1,22 @@
+import json
+import requests as r
+from collections import namedtuple
+
+
+
+def larmslag_sammanfattning():
+    purl = "https://polisen.se/api/events"
+    svar = r.get(purl)
+    foo = json.loads(svar.content, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+    bar = {}
+
+    for i in foo:
+        if i.type not in bar:
+            bar[i.type] = 1
+        else:
+            bar[i.type] += 1
+
+    for i,j in sorted(bar.items()):
+        print(i,": ",j,"stycken")
+larmslag_sammanfattning()
